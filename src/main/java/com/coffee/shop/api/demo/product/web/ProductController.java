7d20bb62602454254a2019a5e138532c7676d78b
Arseniy Kuteynitsyn <arseniy.kuteynitsyn@dataart.com>
2022-05-23 17:26:21 +0200

    fix product filter

--- src/main/java/com/coffee/shop/api/demo/product/web/ProductController.java
+++ src/main/java/com/coffee/shop/api/demo/product/web/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/filter")
-    public Page<ProductDto> find(ProductFilter filter) {
+    public Page<ProductDto> find(@RequestBody ProductFilter filter) {
         return productService.filter(filter);
     }
 
