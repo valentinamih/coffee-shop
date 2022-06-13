fa7d5c3113da1711dd88cd9f85cc395465d9e7af
Arseniy Kuteynitsyn <arseniy.kuteynitsyn@dataart.com>
2022-06-13 02:42:38 +0200

    migrate to kotlin

--- src/main/kotlin/com/coffee/shop/api/demo/image/api/model/Image.kt
+++ src/main/kotlin/com/coffee/shop/api/demo/image/api/model/Image.kt
@@ -1,10 +1,7 @@
 package com.coffee.shop.api.demo.image.api.model
 
-import org.hibernate.annotations.Type
-import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
-import javax.persistence.Lob
 
 @Entity
 class Image {
@@ -12,9 +9,5 @@ class Image {
     var id: Long? = null
     var type: String? = null
 
-    @Lob
-    @Column(columnDefinition = "BLOB")
-    @Type(type = "org.hibernate.type.BinaryType")
-    lateinit var content: ByteArray
     var destination: String? = null
 }
